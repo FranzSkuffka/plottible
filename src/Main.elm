65c436c4d05c1f6d06489a76d4f377753d7c46a1
Jan Wirth <jan.wirth@mercedes-benz.io>
2019-01-04 12:15:34 +0100

    Allow selecting channels

--- src/Main.elm
+++ src/Main.elm
@@ -1,14 +1,22 @@
 module Main exposing (..)
 
 import Browser
-import Html.Styled exposing (Html, text, div, h1, img, toUnstyled, Attribute)
-import Html.Styled.Attributes exposing (src, css)
+import Html.Styled exposing (..)
+import Html.Styled.Attributes as Attribs exposing (src, css, type_, value)
+import Html.Styled.Events exposing (onCheck)
 import Css exposing (..)
 import Dict exposing (Dict)
 import FileSelect exposing (..)
 import Styles
 import File exposing (File)
+import Task
+import Csv
 
+read : File -> Cmd Msg
+read file =
+  Task.perform
+    (FileContentLoaded (File.name file))
+    (File.toString file)
 
 ---- MODEL ----
 
@@ -17,13 +25,14 @@ type alias Model =
   {
     trials : List Trial
   , params : Params
-  , files : Dict String File
+  , files : List FileContent
   , fileSelect : FileSelect.Model
+  , data : Dict String CsvData
   }
 
 type alias Params = {
     range : (Int, Int)
-  , channels : Dict String String
+  , channels : Dict String Bool
   }
 
 type alias Trial = List Channel
@@ -41,21 +50,32 @@ initModel =
   Model
     []
     initParams
-    Dict.empty
+    []
     FileSelect.init
+    Dict.empty
 
 initParams =
   Params
     (-2000, 2000)
     Dict.empty
 
+addChannel : String -> Dict String Bool -> Dict String Bool
+addChannel channelName channelSelection =
+  Dict.update channelName
+  (\sel ->
+    case sel of
+      Just True -> Just True
+      _ -> Just False
+  )
+  channelSelection
+
 
 ---- UPDATE ----
-
-
 type Msg
     = NoOp
     | FileSelectMsg FileSelect.Msg
+    | FileContentLoaded String String
+    | SelectChannel String Bool
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -63,12 +83,58 @@ update msg model =
   case msg of
     FileSelectMsg msg_ ->
       let
-        (model_, cmd) = FileSelect.update FileSelectMsg ["text/plain"] msg_ model.fileSelect
+        (model_, cmd) = FileSelect.update FileSelectMsg ["text/vhdr"] msg_ model.fileSelect
+        cmds = Cmd.batch <| cmd :: List.map read model_.files
       in
-        ( {model | fileSelect = model_}, cmd )
+        ( {model | fileSelect = model_}, cmds )
     NoOp -> (model, Cmd.none)
+    SelectChannel name val ->
+      let
+        p = model.params
+        params = {p | channels = Dict.insert name val p.channels}
+        m =
+          { model
+          | params = params
+          }
+      in
+        (m, Cmd.none)
 
+    FileContentLoaded name content ->
+      let
+        p = model.params
+        parsed = parse content
+        allChannels = List.map Tuple.first parsed
+        params = case String.endsWith ".txt" name of
+          True -> { p | channels = List.foldl addChannel p.channels allChannels}
+          False -> p
+        data = case String.endsWith ".txt" name of
+          True -> Dict.insert name parsed model.data
+          False -> model.data
+        m =
+          { model
+          | files = FileContent name content :: model.files
+          , params = params
+          , data = data
+          }
+      in
+        (m, Cmd.none)
 
+parseRow : List String -> Maybe (String, (List String))
+parseRow cells =
+  case cells of
+    "" :: [] -> Nothing
+    head :: tail -> Just (head, tail)
+    _ -> Nothing
+
+parse : String -> CsvData
+parse content =
+    String.split "\n" content
+      |> List.map (String.split ",")
+      |> List.map parseRow
+      |> List.filterMap identity
+
+type alias CsvData =
+  List (String, List String)
 
 ---- PROGRAM ----
 
@@ -87,10 +153,38 @@ view : Model -> Html Msg
 view model =
   div [] [
     fileSelect model.fileSelect.draggedOver
-  , filesView model.files
+  , filesView model
+  , channels model.params
   ]
 
-filesView _ = text "Files here"
+channels : Params -> Html Msg
+channels params =
+  div [] [
+    h2 [] [text "Channel selection"]
+  , div [] <| List.map channelCheckbox (Dict.toList params.channels)
+  ]
+
+channelCheckbox : (String, Bool) -> Html Msg
+channelCheckbox (name, selected) =
+  div [] [
+    input [Attribs.id ("check-" ++ name), type_ "checkbox", Attribs.checked selected, onCheck (SelectChannel name)] []
+  , label [Attribs.for ("check-" ++ name)] [text name ]
+  ]
+
+filesView : Model -> Html Msg
+filesView model =
+  div [] [
+    h2 [] [text "loaded files"]
+  , div [] <| List.map (\f -> div [] [File.name f |> text]) model.fileSelect.files
+  , h2 [] [text "channels"]
+  -- , channels model.files
+  ]
+
+type alias FileContent = {
+    name : String
+  , content : String
+  }
+
 fileStatusCss : Html.Styled.Attribute Msg
 fileStatusCss =
   css [
@@ -123,19 +217,4 @@ highlightStyles : List Style
 highlightStyles = [
     backgroundColor Styles.color.lightBlue
   , color Styles.color.white
-  ]
-
-filesViewCss = css [
-    padding Styles.unit
-  ]
-
-fileViewCss = css [
-    marginBottom (px 10)
-  , displayFlex
-  ]
-
-fileNameCss = css [
-    fontWeight bold
-  , padding2 (px 5) (px 10)
-  , backgroundColor Styles.color.lightGrey
   ]
